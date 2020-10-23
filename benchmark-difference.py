19ad52265a968a5c472154dfed40ffdc363fee6b
Abhishek Bansal <discover.ab@gmail.com>
2020-10-23 13:16:37 +0530

    + change merge to head

--- benchmark-difference.py
+++ benchmark-difference.py
@@ -7,12 +7,12 @@ def get_result(fileName):
         return dict(reader)
 
 baseResult = get_result('profile-out/benchmark.csv')
-mergeResult = get_result('profile-out-merge/benchmark.csv')
+mergeResult = get_result('profile-out-head/benchmark.csv')
 
 baseMean = baseResult['mean']
-mergeMean = mergeResult['mean']
+headMean = mergeResult['mean']
 
-buildStr = "Branch Head Build Time: " + mergeMean + " | Base Branch Build Time: " + baseMean
+buildStr = "Branch Head Build Time: " + headMean + " | Base Branch Build Time: " + baseMean
 # print result on console and write in a file
 print buildStr
 with open("benchmark-result.txt", 'w') as f:
